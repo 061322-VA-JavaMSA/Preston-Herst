af5355a5ce5914145377de2cd080a56ada5dd362
Preston Herst <phers006@gmail.com>
2022-06-14 15:42:45 -0500

    modifying first practice project

--- RevJavaCode/src/main/java/com/Revature/HelloWorld.java
+++ RevJavaCode/src/main/java/com/Revature/HelloWorld.java
@@ -5,5 +5,22 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello from Eclipse!");
+		
+		
+		//widening casting
+		byte a = 2;
+		int b = a;
+		
+		//narrowing casting
+		double d = 35.5;
+		float f = (float)d;
+		
+		int wholeNumber = 11;
+		//method 1
+		//Integer intWrap = new Integer(wholeNumber);
+		//method 2 
+		Integer intWrap = wholeNumber;
+		
+		
 	}
 }
