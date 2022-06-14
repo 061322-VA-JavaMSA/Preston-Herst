c7fdf254c02c99c33d8250de042e09e598e0792e
Preston Herst <phers006@gmail.com>
2022-06-14 14:49:53 -0500

    random number generator complete

--- week1/java-compilation-activity/randomNum.java
+++ week1/java-compilation-activity/randomNum.java
@@ -2,11 +2,13 @@ public class randomNum{
 	
 	public static void main(String[] args) {
 		
+		System.out.println(args[0]);
+		
 		int x = Integer.parseInt(args[0]);
 		String s = args[1];
 		
-		int i = 1;
-		while(i <= x) {
+		
+		for(int i = 0; i < x; i++) {
 			System.out.println(s);
 		}
 	}
