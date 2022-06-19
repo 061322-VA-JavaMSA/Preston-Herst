8fc9479330cb77e8e11eb8d572cf19de33fd89bf
Preston Herst <phers006@gmail.com>
2022-06-19 15:10:12 -0500

    adding study guide to repo

---
+++ RevJavaCode/src/main/java/com/Revature/ArrayPractice.java
@@ -0,0 +1,15 @@
+package com.Revature;
+
+import java.util.Arrays;
+
+public class ArrayPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] var = {1,2, 3, 4, 5};
+		System.out.println(Arrays.toString(var));
+
+	}
+
+}
