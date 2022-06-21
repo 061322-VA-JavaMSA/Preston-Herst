44135676c27c7b64c0441d4cd949a7e161cf6f15
Preston Herst <phers006@gmail.com>
2022-06-21 00:37:13 -0500

    working on menus for projectZero

---
+++ project0/src/main/java/Shop/CarShop.java
@@ -0,0 +1,55 @@
+package Shop;
+import java.util.Scanner;
+
+public class CarShop {
+
+	static Scanner sc = new Scanner(System.in);
+	
+	
+	public static void main(String[] args) {
+		
+		String username = "";
+		String password = "";
+		int end = 1;
+
+		do {
+			System.out.println("Hello! Welcome to Grizzlies Used Auto Shop");
+			System.out.println();
+			System.out.println("To login, press 1, to create a new account press 2.");
+			
+			System.out.print("option: ");
+			int userChoice = sc.nextInt();
+			System.out.println();
+			
+			if (userChoice == 1) {
+				
+				System.out.println("Please enter your username and password");
+				
+				System.out.print("username: ");
+				username = sc.next();
+				System.out.print("password: ");
+				password = sc.next();	
+				
+				if (username.equals("customer")) {
+//					customerMenu();
+					System.out.println("customer");
+				}
+				else if(username.equals("employee")) {
+					System.out.println("employee");
+				}
+			}
+			else if(userChoice == 2) {
+				
+//				createUser();
+			}
+			else {
+				System.out.println("Exiting shop, goodbye!");
+				end = 0;
+			}
+			System.out.println("username: " + username + "  " + "password: " + password);
+			System.out.println();
+			
+		}while(end != 0);
+		}
+		
+}
